cefda190e1971ccac9cbadf034e31e4ab10b7a84
dbhardwaj1904 <bhardwaj.dhanjay1998@gmail.com>
2021-05-05 19:25:12 +0530

    band_name

---
+++ main.py
@@ -0,0 +1,7 @@
+# Program Band Name Generator - Day 01 of 100 
+
+print("Welcome to the Band Name Generator!!!")
+city_name = input("What's the Name of the City you grew up in ?\n")
+pet_name = input("Who's your Lead Singer or Nick Name of your Lead Singer?\n")
+band_name = city_name + " " + pet_name
+print("\nYour Band Name could be " + band_name + "\n")
